582f87adac82d53f78a0e9cef314ed51f20963ff
yasintaades <harahapyasinta@student.esaunggul.ac.id>
2026-01-20 16:33:22 +0700

    setup landing layout and assets

---
+++ app/(landing)/components/home/categories.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRight } from "react-icons/fi";
+
+const categoryList = [
+  {
+    name: "Running",
+    imgUrl: "category-running.png",
+  },
+  {
+    name: "Tennis",
+    imgUrl: "category-tennis.png",
+  },
+  {
+    name: "Basketball",
+    imgUrl: "category-basketball.png",
+  },
+  {
+    name: "Football",
+    imgUrl: "category-football.png",
+  },
+  {
+    name: "Badminton",
+    imgUrl: "category-badminton.png",
+  },
+  {
+    name: "Swimming",
+    imgUrl: "category-swimming.png",
+  },
+];
+
+const CategoriesSection = () => {
+  return (
+    <section id="category-section" className="container mx-auto pb-20">
+      <div className="flex justify-between">
+        <h2 className="font-bold text-2xl">Browse By Categories</h2>
+        <Link href="#" className="flex gap-2 text-primary font-medium">
+          <span className="self-center">See All Categories</span>
+          <FiArrowRight className="self-center" />
+        </Link>
+      </div>
+      <div className="grid grid-cols-6 gap-12 mt-8">
+        {categoryList.map((category, index) => (
+          <div
+            className="rounded-lg bg-gradient-to-r from-[#F1F1F1] to-[#F7F7F7] w-full aspect-square flex justify-center"
+            key={index}
+          >
+            <div className="self-center">
+              <Image
+                src={`/images/categories/${category.imgUrl}`}
+                width={86}
+                height="86"
+                alt={category.name}
+                className="mb-[10px]"
+              />
+              <div className="text-primary font-medium text-xl text-center">
+                {category.name}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CategoriesSection;
---
+++ app/(landing)/components/home/products.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../ui/button";
+import { FiPlus } from "react-icons/fi";
+
+const productList = [
+  {
+    name: "SportsOn Product 1",
+    category: "Running",
+    price: 450000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 2",
+    category: "Running",
+    price: 250000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 3",
+    category: "Running",
+    price: 230000,
+    imgUrl: "product-3.png",
+  },
+  {
+    name: "SportsOn Product 4",
+    category: "Running",
+    price: 440000,
+    imgUrl: "product-4.png",
+  },
+  {
+    name: "SportsOn Product 5",
+    category: "Running",
+    price: 550000,
+    imgUrl: "product-5.png",
+  },
+  {
+    name: "SportsOn Product 6",
+    category: "Running",
+    price: 650000,
+    imgUrl: "product-6.png",
+  },
+];
+
+const ProductsSection = () => {
+  return (
+    <section id="products-section" className="container mx-auto mt-32">
+      <h2 className="font-bold italic text-4xl text-center mb-11">
+        <span className="text-primary">OUR </span>PRODUCTS
+      </h2>
+      <div className="grid grid-cols-4 gap-5">
+        {productList.map((product, index) => (
+          <Link
+            href="#"
+            key={index}
+            className="p-1.5 bg-white hover:drop-shadow-xl duration-300"
+          >
+            <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
+              <Image
+                src={`/images/products/${product.imgUrl}`}
+                alt={product.name}
+                width={300}
+                height={300}
+                className="aspect-square object-contain"
+              />
+              <Button className="w-10 h-10 p-2! absolute right-3 top-3 ">
+                <FiPlus size={24} />
+              </Button>
+            </div>
+            <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
+            <div className="flex justify-between mb-8">
+              <div className="text-gray-500">{product.category}</div>
+              <div className="font-medium text-primary">
+                {Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                  maximumSignificantDigits: 3,
+                }).format(product.price)}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProductsSection;
