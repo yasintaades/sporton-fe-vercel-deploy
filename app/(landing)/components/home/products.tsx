ca09ba02c6c947424999a48057296325a5c59b01
yasintaades <harahapyasinta@student.esaunggul.ac.id>
2026-02-05 13:05:33 +0700

    session--4

--- app/(landing)/components/home/products.tsx
+++ app/(landing)/components/home/products.tsx
@@ -1,59 +1,81 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import Button from "../ui/button";
 import { FiPlus } from "react-icons/fi";
-import priceFormatter from "@/app/utils/price-formatter";
-import { Product } from "@/app/types";
-import { getImageUrl } from "@/app/lib/api";
-import { useCartStore } from "@/app/hooks/use-cart-store";
 
-type TProductsProps = {
-  products: Product[];
-};
+const productList = [
+  {
+    name: "SportsOn Product 1",
+    category: "Running",
+    price: 450000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 2",
+    category: "Running",
+    price: 250000,
+    imgUrl: "product-1.png",
+  },
+  {
+    name: "SportsOn Product 3",
+    category: "Running",
+    price: 230000,
+    imgUrl: "product-3.png",
+  },
+  {
+    name: "SportsOn Product 4",
+    category: "Running",
+    price: 440000,
+    imgUrl: "product-4.png",
+  },
+  {
+    name: "SportsOn Product 5",
+    category: "Running",
+    price: 550000,
+    imgUrl: "product-5.png",
+  },
+  {
+    name: "SportsOn Product 6",
+    category: "Running",
+    price: 650000,
+    imgUrl: "product-6.png",
+  },
+];
 
-const ProductsSection = ({ products }: TProductsProps) => {
-  const { addItem } = useCartStore();
-
-  const handleAddtoCart = (e: React.MouseEvent, product: Product) => {
-    e.preventDefault();
-    e.stopPropagation();
-    addItem(product);
-  };
-
+const ProductsSection = () => {
   return (
-    <section id="products-section" className="container mx-auto mt-32 mb-52">
+    <section id="products-section" className="container mx-auto mt-32">
       <h2 className="font-bold italic text-4xl text-center mb-11">
         <span className="text-primary">OUR </span>PRODUCTS
       </h2>
       <div className="grid grid-cols-4 gap-5">
-        {products.map((product) => (
+        {productList.map((product, index) => (
           <Link
-            href={`/product/${product._id}`}
-            key={product._id}
+            href="#"
+            key={index}
             className="p-1.5 bg-white hover:drop-shadow-xl duration-300"
           >
             <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
               <Image
-                src={getImageUrl(product.imageUrl)}
+                src={`/images/products/${product.imgUrl}`}
                 alt={product.name}
                 width={300}
                 height={300}
                 className="aspect-square object-contain"
               />
-              <Button
-                className="w-10 h-10 p-2! absolute top-3 right-3 "
-                onClick={(e) => handleAddtoCart(e, product)}
-              >
+              <Button className="w-10 h-10 p-2! absolute right-3 top-3 ">
                 <FiPlus size={24} />
               </Button>
             </div>
             <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
             <div className="flex justify-between mb-8">
-              <div className="text-gray-500">{product.category.name}</div>
+              <div className="text-gray-500">{product.category}</div>
               <div className="font-medium text-primary">
-                {priceFormatter(product.price)}
+                {Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                  maximumSignificantDigits: 3,
+                }).format(product.price)}
               </div>
             </div>
           </Link>
